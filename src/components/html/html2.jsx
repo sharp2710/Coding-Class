a37f09a7702e98d40cd88506b708ad1480f03d9c
sharp <98083474+SharpTH@users.noreply.github.com>
2022-03-11 22:39:36 +0700

    Delete src directory

--- src/components/html/html2.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function html2() {
-  return (
-    <div>html2</div>
-  )
-}
-
-export default html2
--- src/components/html/html3.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function html3() {
-  return (
-    <div>html3</div>
-  )
-}
-
-export default html3
